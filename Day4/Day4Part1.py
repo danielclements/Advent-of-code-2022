f7c1c29306a4102b04b33a29f1fc0dbdafff6511
Daniel Clements <daniel.clements14@gmail.com>
2022-12-08 17:06:18 +0100

    completed day4 part1

---
+++ Day4/Day4Part1.py
@@ -0,0 +1,40 @@
+f = open('Day4\data-set.txt', 'r')
+lines = f.readlines()
+
+duplicate_count = 0
+
+for line in lines:
+    line = line.replace("\n","")
+
+    splitText = line.split(",")
+
+    elf_1 = splitText[0]
+    elf_2 = splitText[1]
+
+    elf1_range_strip = elf_1.split("-")
+    elf1_complete_range = set(range(int(elf1_range_strip[0]), int(elf1_range_strip[1])  + 1))
+
+    elf2_range_strip = elf_2.split("-")
+    elf2_complete_range = set(range(int(elf2_range_strip[0]), int(elf2_range_strip[1]) + 1 ))
+
+
+    result = elf2_complete_range.issubset(elf1_complete_range)
+    result_reversed = elf1_complete_range.issubset(elf2_complete_range)
+
+
+    if result:
+        duplicate_count += 1
+        
+    elif result_reversed:
+        duplicate_count +=1
+        
+    else:
+        print(elf_1 + "----" + elf_2)
+ 
+
+
+
+print(duplicate_count)
+
+    
+    
